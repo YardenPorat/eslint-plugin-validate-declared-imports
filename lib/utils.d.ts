de2269806e07391bc6c783de3fa5e478bf7ef8a6
Yarden Porat <yardenporat@gmail.com>
2022-01-08 12:31:26 +0200

    chore: rebuild

--- lib/utils.d.ts
+++ lib/utils.d.ts
@@ -1,3 +1,9 @@
 import { ESLintUtils } from '@typescript-eslint/experimental-utils';
 export declare function ruleCreator(...args: Parameters<ReturnType<typeof ESLintUtils.RuleCreator>>): import("@typescript-eslint/experimental-utils/dist/ts-eslint/Rule").RuleModule<string, readonly unknown[], import("@typescript-eslint/experimental-utils/dist/ts-eslint/Rule").RuleListener>;
+/**
+ * @param request
+ * @returns true if given string is a node_modules package request
+ */
+export declare function isPackageRequest(request: string): boolean;
+export declare function isRelativeRequest(request: string): boolean;
 //# sourceMappingURL=utils.d.ts.map
